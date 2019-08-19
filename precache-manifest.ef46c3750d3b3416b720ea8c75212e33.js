82faf0ff36ba1492be918441a4ccaf1770c1183a
Daugirdas <kuprionis@gmail.com>
2019-08-19 18:18:35 +0100

    Updates

---
+++ precache-manifest.ef46c3750d3b3416b720ea8c75212e33.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0623fbb3201edfad71d579e6d4c67911",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c421566a1de23992f06",
+    "url": "/portfolio/static/css/main.291677c5.chunk.css"
+  },
+  {
+    "revision": "c284e16ed1ec498dc549",
+    "url": "/portfolio/static/js/2.c87e636a.chunk.js"
+  },
+  {
+    "revision": "3c421566a1de23992f06",
+    "url": "/portfolio/static/js/main.c2c419a5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "79b3c647bcb4502fe1f95e9d9c5de5d9",
+    "url": "/portfolio/static/media/Cloud.79b3c647.svg"
+  },
+  {
+    "revision": "534afba6dc879efc03f27d5c964416bf",
+    "url": "/portfolio/static/media/Cloudy.534afba6.svg"
+  },
+  {
+    "revision": "3694ef67610075498c91cdb7b2bfb2f4",
+    "url": "/portfolio/static/media/Fog.3694ef67.svg"
+  },
+  {
+    "revision": "316ec3310f84545f10f9525b44509d10",
+    "url": "/portfolio/static/media/Rain.316ec331.svg"
+  },
+  {
+    "revision": "20379c89e064dca7173f49395ec16223",
+    "url": "/portfolio/static/media/Snow.20379c89.svg"
+  },
+  {
+    "revision": "1afe7efda2ae788f15823e0f19d4a140",
+    "url": "/portfolio/static/media/Storm.1afe7efd.svg"
+  },
+  {
+    "revision": "53f709c56c0cb268362ba119c34e072a",
+    "url": "/portfolio/static/media/Sun.53f709c5.svg"
+  },
+  {
+    "revision": "6b4d10062fb150d93eb6d0a550d19947",
+    "url": "/portfolio/static/media/Wind.6b4d1006.svg"
+  },
+  {
+    "revision": "18fbd4d51d5587c68d7586a669f7c0f6",
+    "url": "/portfolio/static/media/align-center.18fbd4d5.svg"
+  },
+  {
+    "revision": "7b8757446f0804b2f52e164e4af0ceff",
+    "url": "/portfolio/static/media/align-to-left.7b875744.svg"
+  },
+  {
+    "revision": "ca9c0d7914062d9528447162a193ddd7",
+    "url": "/portfolio/static/media/align-to-right.ca9c0d79.svg"
+  },
+  {
+    "revision": "c681da4656a202b2ecd81bf5cb5d56ab",
+    "url": "/portfolio/static/media/bold.c681da46.svg"
+  },
+  {
+    "revision": "88f8253773c20b92cd972f0a6f8589bb",
+    "url": "/portfolio/static/media/inmobilus-design.88f82537.png"
+  },
+  {
+    "revision": "a0bafb4c7d7e9c0e7ec673a865ce59b2",
+    "url": "/portfolio/static/media/italicize-text.a0bafb4c.svg"
+  },
+  {
+    "revision": "3012b9214d8f7348457bda3912ede410",
+    "url": "/portfolio/static/media/redo-arrow.3012b921.svg"
+  },
+  {
+    "revision": "405e8a80ea8c09871ea450051e6501b0",
+    "url": "/portfolio/static/media/text-editor-app.405e8a80.png"
+  },
+  {
+    "revision": "41cde3615793168ff71e2d2c2739497a",
+    "url": "/portfolio/static/media/underline-text-button.41cde361.svg"
+  },
+  {
+    "revision": "0f28e7fa93444588b737dd2726a986e3",
+    "url": "/portfolio/static/media/undo-arrow.0f28e7fa.svg"
+  },
+  {
+    "revision": "fd878c08b3b2f9acd593f1b8eed77403",
+    "url": "/portfolio/static/media/weather-app.fd878c08.png"
+  }
+]);
